73e492b1262e3749aac051c9e7409206b3ee2c09
fcorte <fcorte@users.noreply.github.com>
2016-01-14 18:42:09 +0000

    new section function with dynamic variable names
    load eventListener not working.

--- Snake.js
+++ Snake.js
@@ -1,4 +1,80 @@
+document.addEventListener('load', ready);
+
 var snake = []
+// creates the snake array. To be populated with sections as the snake gets longer.
+
+
+function ready () {
+
+    for (var i = 40; i>= 10; i = i - 10) {
+        section(i, 200);
+    }
+//  creates the initial body of the snake
+}
+
+/*document.addEventListener("keydown", key);
+// listens for a keydown event and initializes the function key
+
+function key(e) {
+    value = e.keyCode;
+// stores the keyCode of the pressed key in the variable value
+       
+    if (value == 38 || value == 40 || value ==37 || value == 39) {
+// checks if the key is one the of the approved ones
+
+        if (snake.length >= 4) {
+// checks if it is the first time that the user is pressing the key   
+            if (value == 38) {
+               return;
+            }
+            else if (value == 40) {
+               return;
+            }
+            else if (value == 37) {
+               return;
+            }
+            else if (value == 39) {
+               return;
+            }
+         }
+         else {
+            start();
+         }
+    }
+}
+
+*/
+function section (xpos, ypos) {
+   for (var i = 0; i<1; i++) {
+       window['newPart' + i] = document.createElement('div');
+       snake.push('newPart' + i);
+      
+       window['newPart' + i].style.position = "absolute";
+       window['newPart' + i].style.left = xpos+"px";
+       window['newPart' + i].style.top = ypos+"px";
+       window['newPart' + i].style.width = "10px";
+       window['newPart' + i].style.height = "10px";
+       window['newPart' + i].style.backgroundColor = "green";  
+
+       document.body.appendChild(window['newPart' + i]);
+
+   }
+}
+
+/*function start () {
+    
+    var head = section(40,200);
+    snake.push(head);
+//  creates head of the snake and push it into the snake array
+
+    for (var i = 10; i<40; i = i + 10) {
+    snake.push(section(i, 200));
+    }
+//  creates the initial body of the snake
+}*/
+
+
+/* var snake = []
 // creates the snake array. To be populated with sections as the snake gets longer.
 
 document.addEventListener("keydown", key);
@@ -57,7 +133,7 @@ function start () {
     }
 //  creates the initial body of the snake
 }
-
+*/
 
 /*
 var snake = document.getElementById("Snake");
