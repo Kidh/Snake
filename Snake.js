2ff784925f896e60c675e8b20a822964302ef1fe
fcorte <fcorte@users.noreply.github.com>
2016-01-15 13:03:13 +0000

    updated with onload Event. Working on definying movement.

--- Snake.js
+++ Snake.js
@@ -1,53 +1,26 @@
-document.addEventListener('load', ready);
-
 var snake = []
 // creates the snake array. To be populated with sections as the snake gets longer.
 
+window.addEventListener("load", ready);
+// not quite sure why document.addEventListener("load", ready) does not work
 
 function ready () {
 
-    for (var i = 40; i>= 10; i = i - 10) {
-        section(i, 200);
-    }
-//  creates the initial body of the snake
+   for (var i = 40; i>= 10; i = i - 10) {
+       section(i, 200);
+   }
 }
+//  creates the initial body of the snake once the page loads(length = 4);
 
-/*document.addEventListener("keydown", key);
-// listens for a keydown event and initializes the function key
-
-function key(e) {
-    value = e.keyCode;
-// stores the keyCode of the pressed key in the variable value
-       
-    if (value == 38 || value == 40 || value ==37 || value == 39) {
-// checks if the key is one the of the approved ones
-
-        if (snake.length >= 4) {
-// checks if it is the first time that the user is pressing the key   
-            if (value == 38) {
-               return;
-            }
-            else if (value == 40) {
-               return;
-            }
-            else if (value == 37) {
-               return;
-            }
-            else if (value == 39) {
-               return;
-            }
-         }
-         else {
-            start();
-         }
-    }
-}
-
-*/
 function section (xpos, ypos) {
    for (var i = 0; i<1; i++) {
        window['newPart' + i] = document.createElement('div');
+// creates the div that will represent a body part of the snake
+// dynamic variable names are needed in order to be able to later access
+// their properties using the function "move"
+
        snake.push('newPart' + i);
+// push the new part onto the snake array
       
        window['newPart' + i].style.position = "absolute";
        window['newPart' + i].style.left = xpos+"px";
@@ -61,17 +34,34 @@ function section (xpos, ypos) {
    }
 }
 
-/*function start () {
-    
-    var head = section(40,200);
-    snake.push(head);
-//  creates head of the snake and push it into the snake array
+document.addEventListener("keydown", key);
+// listens for a keydown event and initializes the function key
 
-    for (var i = 10; i<40; i = i + 10) {
-    snake.push(section(i, 200));
+function key(e) {
+    value = e.keyCode;
+// stores the keyCode of the pressed key in the variable value
+       
+    if (value == 38 || value == 40 || value ==37 || value == 39) {
+// checks if the key is one the of the approved ones
+   
+        if (value == 38) {
+            for (var l = snake.length - 1; l >= 0; l--) {
+                var currPos = window['newPart' + l].offsetLeft
+                alert(currPos);
+            }
+        }
+        else if (value == 40) {
+            return;
+        }
+        else if (value == 37) {
+            return;
+        }
+        else if (value == 39) {
+            return;
+        }
     }
-//  creates the initial body of the snake
-}*/
+}
+
 
 
 /* var snake = []
